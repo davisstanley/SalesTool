9046e1d4497d4005920773ddce89ecbae30f3412
davis <davis@DavisDesktop>
2020-03-22 09:05:02 -0400

    First change

---
+++ SalesTest/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		
+		System.out.println("Test 1");
+	}
+
+}
